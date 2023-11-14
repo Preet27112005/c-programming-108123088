8311c1a213549eef5a1dddddc143c0f8fd790f33
Preet27112005 <143439456+Preet27112005@users.noreply.github.com>
2023-11-14 17:05:18 +0530

    Add files via upload

---
+++ week6(StrEqual).c
@@ -0,0 +1,34 @@
+int main()
+{
+	int  t,b,n,i;
+	char c;
+	
+	printf("Enter no. of charcters in 2 strings respectively:");
+	scanf("%d %d",&n,&x);
+	
+	char a[n+1];
+	char b[x+1];
+	
+	printf("Enter the 1st string:");
+	scanf("%s",a);
+	
+	printf("Enter the 2st string:");
+	scanf("%s",b);
+	
+	
+	fflush(stdin);
+	
+	if(n==x)
+	{
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==c)
+		{
+		 i++;
+	    }
+	}
+   }
+	else printf("both strings are noy equal");
+	
+	if()
+	return 0;}
